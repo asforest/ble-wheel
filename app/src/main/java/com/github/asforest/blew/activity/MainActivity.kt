5b462aae47683ecd06ceffaaf7f2a5abe90a7465
asforest <asforest@qq.com>
2022-05-27 02:12:16 +0800

    style: increase some heights for top action bar

--- app/src/main/java/com/github/asforest/blew/activity/MainActivity.kt
+++ app/src/main/java/com/github/asforest/blew/activity/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity()
                     finish()
                 }
             }
-            
+
             if (!bluetoothManager.adapter.isEnabled)
             {
                 registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
